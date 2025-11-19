79c3a8cbaccecb8f0ce67a2df99c690c641e6870
Luiz-Felipe-Abreu <luizfelipea.conceicao@gmail.com>
2025-11-18 23:01:35 -0300

    teste commit

--- setup.sh
+++ setup.sh
@@ -71,3 +71,4 @@ az container create \
 DB_FQDN=$(az container show --resource-group "$RG_NAME" --name "$DB_CONTAINER_NAME" --query ipAddress.fqdn -o tsv)
 
 echo "✅ Banco de dados criado com sucesso! ✅"
+echo "teste de commit"
